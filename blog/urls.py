5cb84c298a96b3af56769c31c5e8a77e064ae55b
kanyebek <koshonovkanybek@gmail.com>
2025-04-15 14:06:23 +0600

    HW3

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import test_view,html_view, site_view, post_list_view
+from posts.views import test_view,html_view, site_view, post_list_view, post_detail_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path('html/', html_view),
     path('main/', site_view),
     path('posts/', post_list_view),
-]
+    path('posts/<int:post_id>/', post_detail_view),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,HttpResponse
-import random
 from posts.models import Post
 # Create your views here.
 
@@ -15,3 +14,7 @@ def site_view(request):
 def post_list_view(request):
     posts = Post.objects.all()
     return render(request, 'posts/post_list.html', context = {'posts': posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/post_detail.html', context = {'post': post})
